e8372ddff36e08a8befa950e61119d80a52f6c62
JanneJok <janne.jokela84@gmail.com>
2025-12-08 11:31:19 +0200

    Test pre-commit hook

--- inflation-site-optimized.js
+++ inflation-site-optimized.js
@@ -1,4 +1,4 @@
-// Configuration for proxy server
+// Configuration for proxy server (pre-commit hook test)
 const CONFIG = {
     SHEET_ID: '1tj7AbW3BkzmPZUd_pfrXmaHZrgpKgYwNljSoVoAObx8',
     API_KEY: 'AIzaSyDbeAW-uO-vEHuPdSJPVQwR_l1Axc7Cq7g',
